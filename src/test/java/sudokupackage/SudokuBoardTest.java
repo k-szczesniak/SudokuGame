4d6ea766272317680da72bf1e212d9c4a74d8098
224434 Szcześniak Krzysztof <224434@edu.p.lodz.pl>
2020-03-16 17:24:36 +0100

    Modification TestCorrectFillBoard by using Sets

--- src/test/java/sudokupackage/SudokuBoardTest.java
+++ src/test/java/sudokupackage/SudokuBoardTest.java
@@ -1,10 +1,56 @@
 package sudokupackage;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.security.auth.login.FailedLoginException;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SudokuBoardTest {
+
+    @Test
+    public void TestCorrectFillBoard() {
+        SudokuBoard board = new SudokuBoard();
+        board.fillBoard();
+
+        //check row
+        for (int row = 0; row < 9; row++) {
+            Set<Integer> set = new HashSet<Integer>();
+            for (int col = 0; col < 9; col++) {
+                if(set.add(board.getBoard()[row][col]) == false) {
+                    fail("Row error");
+                }
+            }
+        }
+
+        //check column
+        for (int col = 0; col < 9; col++) {
+            Set<Integer> set = new HashSet<Integer>();
+            for (int row = 0; row < 9; row++) {
+                if(set.add(board.getBoard()[row][col]) == false) {
+                    fail("Column error");
+                }
+            }
+        }
+
+        //check box 3x3
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                Set<Integer> set = new HashSet<Integer>();
+                for (int r = row; r < row + 3; r++) {
+                    for (int c = col; c < col + 3; c++) {
+                        if(set.add(board.getBoard()[r][c]) == false) {
+                            fail("Box error");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     @Test
     public void TestBoardRepeats() {
         SudokuBoard s1 = new SudokuBoard();
@@ -23,45 +69,5 @@ class SudokuBoardTest {
         assertEquals(equal, false);
     }
 
-    @Test
-    public void TestCorrectFillBoard() {
-        SudokuBoard board = new SudokuBoard();
-        board.fillBoard();
 
-        //check row
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                for (int index = col + 1; index < 9; index++) {
-                    if (board.getBoard()[row][col] == board.getBoard()[row][index]) {
-                        fail("Row error"); //fail method from junit assertions
-                    }
-//                    assertNotEquals(board.getBoard()[row][col], board.getBoard()[row][index]);
-                }
-            }
-        }
-
-        //check column
-        for (int col = 0; col < 9; col++) {
-            for (int row = 0; row < 9; row++) {
-                for (int index = row + 1; index < 9; index++) {
-                    if (board.getBoard()[row][col] == board.getBoard()[index][col]) {
-                        fail("Column error");
-                    }
-//                    assertNotEquals(board.getBoard()[row][col], board.getBoard()[index][col]);
-                }
-            }
-        }
-
-        //checkbox
-//        for (int row = 0; row < 9; row += 3) {
-//            for (int col = 0; col < 9; col += 3) {
-//                for (int r = row; r < row + 3; r++) {
-//                    for (int c = col; c < col + 3; c++) {
-//                        if()
-//                    }
-//                    assertNotEquals(board.getBoard()[row][col], board.getBoard()[index][col]);
-//                }
-//            }
-//        }
-    }
 }
