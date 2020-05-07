59deb1f250c2bc31b95126109bb88ab1fb6e3b89
224274 Chojnacki Konrad <224274@edu.p.lodz.pl>
2020-05-07 14:03:20 +0200

    Changes of GameWindow

---
+++ View/src/main/java/sudokupackage/SudokuBoardView.java
@@ -0,0 +1,44 @@
+package sudokupackage;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class SudokuBoardView extends SudokuBoard {
+    private boolean[][] isEditable = new boolean[9][9];
+
+    public SudokuBoardView(SudokuSolver sudokusolver) {
+        super(sudokusolver);
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                isEditable[i][j] = true;
+            }
+        }
+    }
+
+    public boolean getIsEditable(int i, int j){
+        return isEditable[i][j];
+    }
+
+    public void setIsEditable(int i, int j, boolean editable){
+        isEditable[i][j] = editable;
+    }
+
+    public void calculateHiddenPostions(int counter) {
+        Set<Integer> setOfPositions = new HashSet<Integer>();
+        Random rand = new Random();
+        while (counter > 0) {
+            if (setOfPositions.add(rand.nextInt(81))) {
+                counter--;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (setOfPositions.contains(9 * i + j)) {
+                    this.set(i, j, 0);
+                }
+            }
+        }
+    }
+
+}
